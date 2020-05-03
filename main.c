a2ef5308a9053ba3f65c8d3d4c8e7ab665d63ed9
Yusei Ito <yuseikun@Outlook.jp>
2020-05-03 17:53:17 +0900

    USART putc

--- main.c
+++ main.c
@@ -1,12 +1,37 @@
 #include <stdint.h>
 #include "targets/STM32F303K8/system.h"
 
+/*
+*PA_2=TX
+*PA_15=RX
+*/
+
+void configureUSBSerial()
+{
+ RCC->AHBENR |= (1 << 17);
+ GPIOA->MODER = (0b10 << 2 * 2);
+ GPIOA->MODER |= (0b10 << 2 * 15);
+ GPIOA->AFRL = 0b0111 << (4 * 2);
+ GPIOA->AFRH = 0b0111 << (4 * 8);
+
+ // Configure UART
+ RCC->APB1ENR |= (0b1 << 17);
+ USART2->BRR = 8000000L / 115200L;
+ USART2->CR1 |= (0b1 << 3); // Transmitter enable
+ USART2->CR1 |= (0b1 << 2); // Receiver enable
+ USART2->CR1 |= (0b1 << 0); //Enable USART
+}
+
 int main(void)
 {
+
+ // Configure Blink
  RCC->AHBENR |= (1 << 18);
  GPIOB->MODER = (0b01 << 2 * 3); // Set '01' to GPIOB port 3
- GPIOB->OTYPER = 0x0000;         //All-push pull
- GPIOB->PUPDR = 0x000000000;     //No pullup-pulldown
+ GPIOB->OTYPER = 0x0000;         // All-push pull
+ GPIOB->PUPDR = 0x000000000;     // No pullup-pulldown
+
+ configureUSBSerial();
 
  int i = 1;
 
@@ -24,6 +49,8 @@ int main(void)
    {
     GPIOB->ODR |= (1 << 3);
    }
+
+   USART2->TDR = 'A';
   }
   i++;
  }
--- targets/STM32F303K8/system.h
+++ targets/STM32F303K8/system.h
@@ -6,6 +6,7 @@
 #define USART1 ((USART_t *)USART_1_BASE)
 #define USART2 ((USART_t *)USART_2_BASE)
 #define RCC ((RCC_t *)RCC_BASE)
+#define GPIOA ((GPIO_t *)GPIO_A_BASE)
 #define GPIOB ((GPIO_t *)GPIO_B_BASE)
 
 int systemInit()
