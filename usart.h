9bc0e494da695ce208b172803bcef48d1e74c5a6
Yusei Ito <yuseikun@Outlook.jp>
2020-05-17 18:35:10 +0900

    Refactoring

--- usart.h
+++ usart.h
@@ -1,13 +1,28 @@
 #include "targets/STM32F303K8/system.h"
 #include "structures/ring_buffer.h"
+#include "lib.h"
 
 ringBuffer_t buf;
 
-void putstr(char *str, uint16_t len)
+void flushc()
+{
+ uint8_t c;
+ ringBuffer_result_t res = pop(&buf, &c);
+ if (res == OK)
+ {
+  USART2->TDR = c;
+ }
+}
+
+void putstrn(char *str, uint16_t len)
 {
 
  uint8_t q = 0;
  ringBuffer_result_t res = push(&buf, str[0]);
+
+ uint8_t flg = 0;
+ if ((USART2->ISR & 0b1 << 7) != 0)
+  flg = 1;
 
  while (res == OK && q < len)
  {
@@ -15,7 +30,15 @@ void putstr(char *str, uint16_t len)
   q++;
  }
 
- USART2->TDR = 'A';
+ if (flg == 1)
+ {
+  flushc();
+ }
+}
+
+void putstr(char *str)
+{
+ putstrn(str, StrLen(str));
 }
 
 int usart2_handler(uint8_t o)
@@ -24,14 +47,7 @@ int usart2_handler(uint8_t o)
  {
   USART2->ICR = 0b1 << 6;
 
-  uint8_t a;
-
-  ringBuffer_result_t res = pop(&buf, &a);
-
-  if (res == OK)
-  {
-   USART2->TDR = a;
-  }
+  flushc();
  }
  else if ((USART2->ISR & (uint32_t)(0b1 << 5))) //RXNE bit
  {
