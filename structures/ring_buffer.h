0de79da7a40e5037800de8222b38518791c0f530
Yusei Ito <yuseikun@Outlook.jp>
2020-05-17 18:10:54 +0900

    Selfmade ringbuffer implementation

---
+++ structures/ring_buffer.h
@@ -0,0 +1,86 @@
+#include <stdint.h>
+
+#define BUFFER_LENGTH 256
+typedef enum
+{
+ OK = 0,
+ buffer_empty = 1,
+ buffer_overflow = 2
+} ringBuffer_result_t;
+
+typedef enum
+{
+ empty = 0,
+ queued = 1,
+ full = 2
+} ringBuffer_state_t;
+
+typedef struct
+{
+ uint16_t length;
+ uint8_t data[BUFFER_LENGTH];
+ uint16_t read_index;
+ uint16_t write_index;
+ ringBuffer_state_t state;
+} ringBuffer_t;
+
+void evaluateState(ringBuffer_t *buf)
+{
+ if (buf->read_index == buf->write_index - 1 || (buf->read_index == buf->length - 1 && buf->write_index == 0))
+ {
+  buf->state = empty;
+ }
+ else if (buf->read_index == buf->write_index + 1 || (buf->write_index == buf->length - 1 && buf->read_index == 0))
+ {
+  buf->state = full;
+ }
+ else
+ {
+  buf->state = queued;
+ }
+}
+
+void ringBuffer_init(ringBuffer_t *buf)
+{
+ buf->length = BUFFER_LENGTH;
+ buf->read_index = 0;
+ buf->write_index = 1;
+
+ evaluateState(buf);
+}
+
+ringBuffer_result_t push(ringBuffer_t *buf, uint8_t data)
+{
+ evaluateState(buf);
+ if (buf->state < full)
+ {
+  buf->data[buf->write_index] = data;
+  buf->write_index++;
+  buf->write_index %= buf->length;
+
+  evaluateState(buf);
+
+  return OK;
+ }
+ else
+ {
+  // Full
+  return buffer_overflow;
+ }
+}
+
+ringBuffer_result_t pop(ringBuffer_t *buf, uint8_t *res)
+{
+ evaluateState(buf);
+ if (buf->state > empty)
+ {
+  buf->read_index++;
+  buf->read_index %= buf->length;
+  *res = buf->data[buf->read_index];
+  buf->data[buf->read_index] = 0x00;
+  evaluateState(buf);
+  return OK;
+ }
+
+ return buffer_empty;
+}
